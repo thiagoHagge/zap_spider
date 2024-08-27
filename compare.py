b4f1fef3fac9734fdd868ff785fd37d7071d5655
Thiago Hagge <thiagohagge09@gmail.com>
2024-08-26 22:42:49 -0300

    first commit

---
+++ compare.py
@@ -0,0 +1,20 @@
+import cv2
+from os import walk
+from os import replace 
+user_path = './zap_users'
+customer_path = './customer'
+for (dirpath, dirnames, filenames) in walk(customer_path):
+    if len(filenames) == 0:
+        print('No customer founded')
+        exit()
+    customer_image = cv2.imread(customer_path+'/'+filenames[0])
+
+    for (dirp, dirn, filen) in walk(user_path):
+        print('No users founded')
+        exit()
+        u_path = user_path+'/'+filen[0]
+        user_image = cv2.imread(u_path)
+        difference = cv2.subtract(customer_image, user_image)
+        b, g, r = cv2.split(difference)
+        if cv2.countNonZero(b) == 0 and cv2.countNonZero(g) == 0 and cv2.countNonZero(r) == 0:
+            replace(u_path, './guilty/'+filen[0])
---
+++ zap.py
@@ -0,0 +1,32 @@
+import requests
+import shutil
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException  
+prompt = input('Digite o número inicial (somente número incluindo ddi):\n')
+n = input('Insira a quantidade de números da busca:\n')
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=options)
+driver.get("https://web.whatsapp.com")
+input("Autentique o whatsapp e na sequêcia aperte Enter")
+#testar caso de numero não existente
+for x in range(int(n)):
+ phone = str(int(prompt)+x)
+ driver.get("https://web.whatsapp.com/send/?phone="+phone)
+ driver.implicitly_wait(90)
+ try:
+  driver.find_element(By.CSS_SELECTOR, '[title="Dados de perfil"]').click()
+  driver.implicitly_wait(10)
+  name = driver.find_element(By.CSS_SELECTOR,'.copyable-text[dir="auto"]').text
+  imgs = driver.find_elements(By.TAG_NAME, 'img')
+  for img in imgs:
+   src = img.get_attribute('src')
+   if src.find('stp=dst-jpg_s96x96') == -1 and src.find('https://') == 0 and src.find('maps') == -1:
+    print('salvando imagem do número:'+phone+'. Nome: '+name)
+    response = requests.get(src, stream=True)
+    with open('./zap_users/'+phone+'-'+name+'.png', 'wb') as out_file:
+     shutil.copyfileobj(response.ra, out_file)
+    del response
+ except NoSuchElementException:
+    c=0 
