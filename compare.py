bfb3b956228ca59b2748146cb5eeb713bde8c9c1
Thiago Hagge <thiagohagge09@gmail.com>
2024-08-30 00:00:49 -0300

    Update compare.py

--- compare.py
+++ compare.py
@@ -1,25 +1,29 @@
 import cv2
-from os import walk
-from os import replace 
+import os
 import numpy as np
 user_path = './zap_users'
 customer_path = './customer'
-for (dirpath, dirnames, filenames) in walk(customer_path):
+guilty_counter = 0
+for (dirpath, dirnames, filenames) in os.walk(customer_path):
     if len(filenames) == 0:
         print('No customer founded')
         exit()
-    customer_image = cv2.resize(cv2.imread(customer_path+'/'+filenames[0]), (224, 224)).astype(np.float32)
+    for name in filenames:
+        customer_image = cv2.resize(cv2.imread(customer_path+'/'+name), (224, 224)).astype(np.float32)
 
-    for (dirp, dirn, filen) in walk(user_path):
-        if len(filen) == 0:
-            print('No users founded')
-            exit()
-        u_path = user_path+'/'+filen[0]
-        user_image = cv2.resize(cv2.imread(u_path), (224, 224)).astype(np.float32)
-        difference = cv2.subtract(customer_image, user_image)
-        b, g, r = cv2.split(difference)
-        if cv2.countNonZero(b) == 0 and cv2.countNonZero(g) == 0 and cv2.countNonZero(r) == 0:
-            replace(u_path, './guilty/'+filen[0])
-            print('igual')
-        else:
-            print('diferente')
+        for (dirp, dirn, filen) in os.walk(user_path):
+            if len(filen) == 0:
+                print('No users founded')
+                exit()
+            for n in filen:
+                u_path = user_path+'/'+n
+                user_image = cv2.resize(cv2.imread(u_path), (224, 224)).astype(np.float32)
+                difference = cv2.subtract(customer_image, user_image)
+                b, g, r = cv2.split(difference)
+                if cv2.countNonZero(b) == 0 and cv2.countNonZero(g) == 0 and cv2.countNonZero(r) == 0:
+                    path = './guilty'
+                    if not os.path.exists(path):
+                            os.makedirs(path)
+                    os.replace(u_path, path+'/'+n)
+                    guilty_counter = guilty_counter + 1
+print(str(guilty_counter)+' guilty found')
